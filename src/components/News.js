c24c60175755f3871f4248716a656a6a6c9a4c11
tsering651 <tseringwang4039@gmail.com>
2022-10-30 16:44:13 +0530

    Fetched data from the api and populated in my appplication

--- src/components/News.js
+++ src/components/News.js
@@ -2,22 +2,43 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export default class News extends Component {
+
+ async componentDidMount(){
+     let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=7b7195fd9d964b35b8ac281aa47d7d43"
+     let data= await fetch(url);
+     let parsedData=await data.json();
+     console.log(parsedData);
+     this.setState({articles:parsedData.articles})
+}
+
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+    };
+  }
   render() {
     return (
       <div className="container my-3">
         <h2>Trending-News headlines</h2>
         <div className="row">
-          <div className="col-md-4">
-            <NewsItem title="MyTitLE" description="how about you" />
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="MyTitLE" description="how about you" />
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="MyTitLE" description="how about you" />
-          </div>
+          {this.state.articles.map((element) => {
+            return <div className="col-md-4"key={element.url}>
+                <NewsItem
+                  url={element.url}
+                  title={element.title}
+                  description={element.description}
+                  imageUrl={element.urlToImage}
+                />
+              </div>
+            
+          })}
         </div>
-       
+        <div className="container d-flex justify-content-between">
+         <button type="button" class="btn btn-primary my-3"> &larr; Previous</button>
+         <button type="button" class="btn btn-primary my-3">Next &rarr;</button>
+        </div>
       </div>
     );
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,18 +2,18 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    let{title,description}=this.props;
+    let{title,description,imageUrl,url}=this.props;
     return (
       <div >
         <div class="card" style={{width:"18rem"}}>
-          <img src="https://ichef.bbci.co.uk/news/1024/branded_news/6ABA/production/_127422372_gettyimages-1244317110.jpg" class="card-img-top" alt="..." />
+          <img src={imageUrl} class="card-img-top" alt="..." />
           <div class="card-body">
             <h5 class="card-title">{title}</h5>
             <p class="card-text">
              {description}
             </p>
-            <a href="#" class="btn btn-primary">
-              Go somewhere
+            <a href={url} target="_blank"class="btn btn-primary" >
+              Read More
             </a>
           </div>
         </div>
